1fa9960f087188b452a7228c5b0b66abcecc92e9
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2022-01-30 14:43:39 +0500

    frontend

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import Front from './Components/FrontPage/Front';
+import Explore from './Components/ExplorePage/Explore';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Front />
+    </div>
+  );
+} 
+
+export default App;
---
+++ front/src/Components/ExplorePage/Explore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./explore.css"
+
+const Explore = () => {
+    return (
+        <>
+
+        <p>hello</p>
+        </>
+    )
+}
+
+export default Explore
