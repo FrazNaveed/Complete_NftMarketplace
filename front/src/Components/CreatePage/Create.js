9bdee37dac064fcb5beb3bce8f4617773a7e6cc3
FrazNaveed <01-134181-021@student.bahria.edu.pk>
2022-04-16 11:24:38 +0500

    create section complete

--- front/src/Components/CreatePage/Create.js
+++ front/src/Components/CreatePage/Create.js
@@ -4,17 +4,15 @@ import "./create.css";
 const Create = () => {
   return (
     <div className="container">
-        <div className="imageSection">
-          <input type="file" accept="video/*" />
-        </div>
-
-        <div className="inputSection">
-          <h2>Create an NFT</h2>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <button>MINT</button>
-        </div>
+      <h1>Create an NFT</h1>
+      <form>
+        <input type="file" placeholder="Select File"/>
+        <input type={Text} placeholder="Title" />
+        {/* Check to take it in number or text in API */}
+        <input type={Text} placeholder="Price" />  
+        <textarea placeholder="Description"></textarea>
+        <button>Mint</button>
+      </form>
     </div>
   );
 };
