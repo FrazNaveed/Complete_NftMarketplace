c897395a81d7f86d370246f0371b16042f7bd901
FrazNaveed <01-134181-021@student.bahria.edu.pk>
2022-05-03 13:53:33 +0500

    cards mapped on frontend

--- front/src/Components/ExplorePage/Explore.js
+++ front/src/Components/ExplorePage/Explore.js
@@ -4,21 +4,24 @@ import video from "../.././Assets/sample.mp4";
 import { Link } from "react-router-dom";
 import { pink } from "@material-ui/core/colors";
 import { fontWeight } from "@mui/system";
+
+
 const Explore = () => {
 
   let arr = [1,2,4,5,6,5,6,7,8, 7,8];
 
   return (
     <>
-      <ul> <li><h1>Explore</h1></li></ul>
+      <ul style={{paddingLeft: "120px", marginTop:"50px", textDecoration:"none"}}> <li><h1>Explore</h1></li></ul>
 
-     <div className="container">
+     <div id="containerStyle" >
      {
         arr.map(()=>{
           return(
             <div className="card"> 
-            <video src={video} />   
-            <Link to="/details">
+            <video src="https://gateway.pinata.cloud/ipfs/QmTv2Tx9XQeLrvg8rs9LCCih6FrHt2mXs3LVBt23ZD7eE7" className="vid" />   
+    
+            {/* <Link to="/details"> */}
                
                <div className="cardDetails">
                <img src="https://picsum.photos/50/50" />
@@ -27,7 +30,7 @@ const Explore = () => {
                </div>
               <p> lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum
               </p>
-              </Link>
+              {/* </Link> */}
             </div>
           );
         })
